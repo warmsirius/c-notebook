283d05213fce64090b11f120b9974a0e503cc306
yuanjun <861022418@qq.com>
2020-09-30 14:55:15 +0800

    0930: 一元二次方程解

--- chapter1/1.一元二次方程解.c
+++ chapter1/1.一元二次方程解.c
@@ -7,18 +7,22 @@
 
 int main(void) {
     int a = 1;
-    int b = 3;
-    int c = 2;
+    int b = 2;
+    int c = 1;
 
     int delta;
     delta = b * b - 4 * a * c;
 
+    double x1, x2;
     if (delta > 0) {
         double x;
         x = sqrt(delta);
-        printf("该方程2个解");
+        x1 = (-b + x) / 2 * a;
+        x2 = (-b - x) / 2 * a;
+        printf("该方程2个解: x1=%.2f, x2=%.2f", x1, x2);
     } else if (delta == 0) {
-        printf("该方程1个解");
+        x1 = x2 = -b / 2 * a;
+        printf("该方程1个解: x1=x2=%.2f", x1);
     } else {
         printf("该方程无解");
     }
