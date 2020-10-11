2c6bddc885567e20eecc02f0cf44955ab89a89a4
yuanjun <861022418@qq.com>
2020-10-11 11:24:10 +0800

    1010: 常量

---
+++ chapter1/4.常量.c
@@ -0,0 +1,24 @@
+//
+// Created by 袁俊 on 2020/10/11.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    // 浮点数常量
+    float f1 = 123.45e-2F;
+    printf("%f\n", f1);
+    // 整数常量
+    int i1 = 1024;
+    printf("%d\n", i1);
+
+    int i2 = 01024;
+    printf("%d\n", i2);
+
+    int i3 = 0X1024;
+    printf("%d\n", i3);
+
+
+    //字符常量
+    char c1 = 'a';
+}
